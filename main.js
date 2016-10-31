7857701c35e3ab5a176d12b86f1d53bb2f1fd98b
borgchrist1 <christian.borg@outlook.com>
2016-10-31 21:31:44 +0100

    added js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 'use strict';
-
-let getClass = document.querySelector(card);
+console.log('hello');
+let getClass = document.querySelectorAll(card);
+for (var i = 0; i < getClass.length; i++){
+console.log(getClass[i]);
+}
