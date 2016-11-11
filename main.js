e807fba284ebb1fe4bb0bf2e96c1bdf9d13e1257
Christian Borg <christian.borg@outlook.com>
2016-11-11 18:01:35 +0100

    Added basa html, css, js

--- main.js
+++ main.js
@@ -1,6 +1,60 @@
 'use strict';
-console.log('hello');
-let getClass = document.querySelectorAll(card);
-for (var i = 0; i < getClass.length; i++){
-console.log(getClass[i]);
+let cardImg;
+let cardBack;
+let pair;
+let cards = [];
+
+
+function creatCard(cardImg, cardBack, pair){
+  let card = {};
+  card.cardImg = '';
+  card.cardBack = 'back';
+  card.pair = false;
+  cards.push(card);
+
 }
+
+
+
+function numberOfCards(newCards){
+  for (var i = 0; i < newCards; i++) {
+    creatCard(cardImg, cardBack, pair);
+  }
+  addValues(cards, newCards);
+}
+
+
+
+function addValues(cards, newCards){
+  let value = 0;
+  for (var i = 0; i < cards.length; i++) {
+    value++;
+    if (value > newCards/2) {
+      value = 1;
+    }
+    cards[i].cardImg = value;
+
+  }
+  mixCards(cards);
+}
+
+function mixCards(cards){
+  for (let i = cards.length; i; i--) {
+    let j = Math.floor(Math.random() * i);
+    [cards[i - 1], cards[j]] = [cards[j], cards[i - 1]];
+  }
+
+}
+
+
+function putCardsOnTable(cards){
+
+}
+
+function checkForPair(){
+
+
+}
+
+numberOfCards(6);
+console.log(cards);
